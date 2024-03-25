c0f01681c7cd3a22707be298c3fd23cda234571c
victorfake <victorhuebr173@gmail.com>
2024-03-25 14:55:40 -0300

    componete livro criado

--- components/Book.jsx
+++ components/Book.jsx
@@ -1,11 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const Book = () => {
+const Book = (props) => {
   return (
     <View>
-        <Text>Nome do Livro é {props.nome}</Text>
-        <Text></Text>
+        <Text>Titulo: {props.nome ? props.nome: "Não Informado"}</Text>
+        <Text>Gênero: {props.genero1 ? props.genero1: "Não Informado"}, {props.genero2 ? props.genero2: "Não Informado"}, {props.genero3 ? props.genero3: ""}</Text>
+        <Text>Preço: {props.preço ? props.preço: "Não Informado"}</Text>
     </View>
   )
 }
