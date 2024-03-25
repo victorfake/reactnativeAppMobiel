f95cd6d82df768d312f075332c8dfdab141bca67
victorfake <victorhuebr173@gmail.com>
2024-03-25 14:55:23 -0300

    componete livro criado

--- App.jsx
+++ App.jsx
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
 import Person from './components/Person';
+import Book from './components/Book';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      
+      
+      
+      <Text style={styles.title}>Venda de Livros</Text>
 
-      <Person menor></Person>
-    </View>
+      <Image
+        source={
+          require('./image/Livros Senhor dos Aneis.png')
+        }
+       
+        />
+    <Book 
+    nome="Triologia Senhor dos aneis" 
+    genero1=" Literatura fantástica" 
+    genero2=" Alta fantasia" 
+    genero3=" Ficção de aventura" 
+    preço="150,00" />
+            
+      </View>
   );
 }
 
@@ -18,4 +35,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  
 });
